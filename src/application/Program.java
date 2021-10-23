573cecdd731183bb4617d08cd042665c896ee957
ThallesGMocellin <thalles.g.mocellin@outlook.com>
2021-10-23 14:11:34 -0300

    Chess layer and print the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,10 @@
 package application;
-import boardgame.Board;
+import chess.ChessMatch;
 public class Program {
 	public static void main(String[] args) {
-		Board board = new Board(8, 8);
+		//Comentários
 		
+		ChessMatch chessMatch = new ChessMatch(null);
+		UI.printBoard(chessMatch.getPieces());		
 	}
-
 }
